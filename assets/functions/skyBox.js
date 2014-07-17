6b87acd25b502ca2f08ab77e7ddfbaf1645b3a29
stefanomaga <ste.maga@stud.uniroma3.it>
2014-07-17 15:38:13 +0200

    final_project

--- assets/functions/skyBox.js
+++ assets/functions/skyBox.js
@@ -17,3 +17,23 @@ function makeSkyBox() {
 	skyBox.position.set(0,0,0);
 	return skyBox;
 }
+
+function makeSkyBoxDay() {
+
+	var imagePrefix = "assets/textures/lostatseadyay/lostatseaday_back";
+	var directions  = ["_back", "_front", "_left", "_right", "_tot", "_bot"];
+	var imageSuffix = ".jpg";
+	var skyGeometry = new THREE.BoxGeometry( 100, 100, 100 );
+
+	
+	var materialArray = [];
+	for (var i = 0; i < 6; i++)
+		materialArray.push( new THREE.MeshBasicMaterial({
+			map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+			side: THREE.BackSide
+		}));
+	var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
+	var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+	skyBox.position.set(0,0,0);
+	return skyBox;
+}
