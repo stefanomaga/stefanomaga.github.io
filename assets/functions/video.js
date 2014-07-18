1cfb97cb43ef85c390105ef8daa48543c5f2aa69
stefanomaga <ste.maga@stud.uniroma3.it>
2014-07-18 16:42:29 +0200

    final_project

--- assets/functions/video.js
+++ assets/functions/video.js
@@ -395,7 +395,7 @@ function makeRemoteControl(){
 	}
 
 	remoteControl_Box.rotation.y = Math.PI/2;
-	remoteControl_Box.position.set(6,3,0.5);
+	remoteControl_Box.position.set(10,10,0.5);
 	var helper = new THREE.AxisHelper(100);
 	remoteControl_Box.add(helper);
 
