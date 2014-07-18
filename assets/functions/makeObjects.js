12d46fab045ceb9b6024b3ebc4983f8d5e78f3b5
stefanomaga <ste.maga@stud.uniroma3.it>
2014-07-19 01:47:05 +0200

    final_project

--- assets/functions/makeObjects.js
+++ assets/functions/makeObjects.js
@@ -1,6 +1,15 @@
 function makeObjects() {
 	objects3D = new THREE.Object3D();
 
+	var toasGeometry = new THREE.BoxGeometry(0.25,0.25,0.25);
+	var toasMaterial = new THREE.MeshLambertMaterial({transparent: true, opacity: 1});
+	toast1 = new THREE.Mesh(toasGeometry, toasMaterial);
+	toast2 = new THREE.Mesh(toasGeometry, toasMaterial);
+	toast1.position.set(6.15,6.75,1.5);	
+	toast2.position.set(5.9,6.75,1.5);	
+	objects3D.add(toast1);
+	objects3D.add(toast2);
+
 	// //LIVING ROOM
 	// objLoader('L_shaped_sofa', 0.012, 13,7.3,0, Math.PI/2,-Math.PI,0);
 	// objLoader('contemp_living_room', 0.012, 13,9.5,0.2, Math.PI/2,0,0);
--- assets/functions/toaster.js
+++ assets/functions/toaster.js
@@ -1,31 +1,12 @@
 function makeToaster() {
-	var toaster3D = new THREE.Object3D();
-
-	makeToasts();
-
-	toaster3D.add(toast1);
-	toaster3D.add(toast2);
-	
 	var toasterGeometry = new THREE.BoxGeometry(0.5,0.5,0.5);
 	var toasterMaterial = new THREE.MeshLambertMaterial({transparent: true, opacity: 1});
 	toaster = new THREE.Mesh(toasterGeometry, toasterGeometry);
 	toaster.position.set(6,6.75,1);
 
-
-	toaster3D.add(toaster);
-	
 	toaster.interact = function() {
 
 	}
 
-	return toaster3D;
-}
-
-function makeToasts() {
-	var toasGeometry = new THREE.BoxGeometry(0.25,0.25,0.25);
-	var toasMaterial = new THREE.MeshLambertMaterial({transparent: true, opacity: 1});
-	toast1 = new THREE.Mesh(toasGeometry, toasMaterial);
-	toast2 = new THREE.Mesh(toasGeometry, toasMaterial);
-	toaster.position.set(6.15,6.75,1.5);	
-	toaster.position.set(5.9,6.75,1.5);	
+	return toaster;
 }
