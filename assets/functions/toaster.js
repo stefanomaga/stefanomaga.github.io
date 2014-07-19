5fe1818dd678325e595001cca0d71953a0406aa6
stefanomaga <ste.maga@stud.uniroma3.it>
2014-07-19 02:06:24 +0200

    final_project

--- assets/functions/toaster.js
+++ assets/functions/toaster.js
@@ -1,12 +1,22 @@
 function makeToaster() {
-	
+
 	var toasterGeometry = new THREE.BoxGeometry(0.5,0.5,0.5);
 	var toasterMaterial = new THREE.MeshLambertMaterial({transparent: true, opacity: 1});
 	toaster = new THREE.Mesh(toasterGeometry, toasterMaterial);
 	toaster.position.set(6,6.75,1);
 
 	toaster.interact = function() {
+		var finishToast1 = new TWEEN.Tween(toast1.position)
+            .to({ x: toast1.position.x, y: toast1.position.y + 1 , z: toast1.position.z }, 2000)
+            //.easing(TWEEN.Easing.Linear.None)
+            .easing(TWEEN.Easing.Bounce.Out)  
+            .start();
 
+        var finishToast2 = new TWEEN.Tween(toast2.position)
+            .to({ x: toast2.position.x, y: toast2.position.y + 1 , z: toast2.position.z }, 2000)
+            //.easing(TWEEN.Easing.Linear.None)
+            .easing(TWEEN.Easing.Bounce.Out)  
+            .start();
 	}
 
 	return toaster;
