ccf5160a38b22bdea873b0f0d98178feaf32d4d2
stefanomaga <ste.maga@stud.uniroma3.it>
2014-07-19 10:49:04 +0200

    final_project

--- assets/functions/transformer.js
+++ assets/functions/transformer.js
@@ -20,7 +20,7 @@ function moveCar(){
             .start(); 
 
 	var movement2 = new TWEEN.Tween(car.position)
-            .to({ x: car.position.x + 15, y: car.position.y + 15, z: car.position.z}, 500)
+            .to({ x: car.position.x + 15, y: car.position.y, z: car.position.z}, 500)
             .easing(TWEEN.Easing.Linear.None)
             //.easing(TWEEN.Easing.Bounce.Out)  
             .delay(750) 
