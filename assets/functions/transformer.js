6c1d697d27cc2cebe428690466fafab2f75bc8cb
stefanomaga <ste.maga@stud.uniroma3.it>
2014-07-19 10:22:31 +0200

    final_project

--- assets/functions/transformer.js
+++ assets/functions/transformer.js
@@ -20,14 +20,14 @@ function moveCar(){
 
     rotationCar = -Math.PI/2;
 
-    var rotation1 = new TWEEN.Tween(car.rotation)
-            .to({ z: -Math.PI/2}, 500)
-            .easing(TWEEN.Easing.Linear.None)
-            //.easing(TWEEN.Easing.Bounce.Out) 
-            .delay(1000) 
-            .start();            
+    // var rotation1 = new TWEEN.Tween(car.rotation)
+    //         .to({ z: -Math.PI/2}, 500)
+    //         .easing(TWEEN.Easing.Linear.None)
+    //         //.easing(TWEEN.Easing.Bounce.Out) 
+    //         .delay(1000) 
+    //         .start();            
 
-    rotationCar = -Math.PI;
+    // rotationCar = -Math.PI;
 }
 
 function resetCar(){
