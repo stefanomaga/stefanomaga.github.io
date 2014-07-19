13a6b69815c38e1f734fde4ee5ace56ab36c536a
stefanomaga <ste.maga@stud.uniroma3.it>
2014-07-19 10:14:14 +0200

    final_project

--- assets/functions/transformer.js
+++ assets/functions/transformer.js
@@ -4,10 +4,17 @@ apartment.add(car);
 function moveCar(){
 	console.log("MOVE CAR");
 	var movement1 = new TWEEN.Tween(car.position)
-            .to({ x: car.position.x + 30, y: car.position.y , z: car.position.z}, 500)
+            .to({ x: car.position.x + 20, y: car.position.y , z: car.position.z}, 500)
             .easing(TWEEN.Easing.Linear.None)
             //.easing(TWEEN.Easing.Bounce.Out)  
             .start();
+
+	var rotation1 = new TWEEN.Tween(car.rotation)
+            .to({ z: Math.PI/2}, 500)
+            .easing(TWEEN.Easing.Linear.None)
+            //.easing(TWEEN.Easing.Bounce.Out) 
+            .delay(500) 
+            .start();            
 }
 
 function resetCar(){
