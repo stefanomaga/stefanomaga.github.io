5904c868db29de95685b838905ad701e8ebb6c45
stefanomaga <ste.maga@stud.uniroma3.it>
2014-07-19 02:00:20 +0200

    final_project

--- assets/functions/toaster.js
+++ assets/functions/toaster.js
@@ -1,11 +1,12 @@
 function makeToaster() {
+	
 	var toasterGeometry = new THREE.BoxGeometry(0.5,0.5,0.5);
 	var toasterMaterial = new THREE.MeshLambertMaterial({transparent: true, opacity: 1});
-	toaster = new THREE.Mesh(toasterGeometry, toasterGeometry);
+	toaster = new THREE.Mesh(toasterGeometry, toasterMaterial);
 	toaster.position.set(6,6.75,1);
 
 	toaster.interact = function() {
-		
+
 	}
 
 	return toaster;
