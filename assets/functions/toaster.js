dd0f3c244d557d3bbbe73f88a773a9c8cfc2a9e1
stefanomaga <ste.maga@stud.uniroma3.it>
2014-07-19 02:19:47 +0200

    final_project

--- assets/functions/toaster.js
+++ assets/functions/toaster.js
@@ -16,11 +16,13 @@ function makeToaster() {
         toast1.position.set(toast1.position.x,toast1.position.y,toast1.position.z+1);
             
         var finishToast1b = new TWEEN.Tween(toast1.position)
-            .to({ x: toast1.position.x, y: toast1.position.y , z: toast1.position.z}, 2000)
+            .to({ x: toast1.position.x, y: toast1.position.y , z: toast1.position.z - 1}, 2000)
             //.easing(TWEEN.Easing.Linear.None)
             .easing(TWEEN.Easing.Bounce.Out) 
             .delay(2000) 
             .start();   
+
+        toast1.position.set(toast1.position.x,toast1.position.y,toast1.position.z-1);   
 
         var finishToast2a = new TWEEN.Tween(toast2.position)
             .to({ x: toast2.position.x, y: toast2.position.y , z: toast2.position.z + 1 }, 2000)
@@ -31,11 +33,13 @@ function makeToaster() {
         toast2.position.set(toast2.position.x,toast2.position.y,toast2.position.z+1);   
 
         var finishToast2b = new TWEEN.Tween(toast2.position)
-            .to({ x: toast2.position.x, y: toast2.position.y , z: toast2.position.z }, 2000)
+            .to({ x: toast2.position.x, y: toast2.position.y , z: toast2.position.z - 1}, 2000)
             //.easing(TWEEN.Easing.Linear.None)
             .easing(TWEEN.Easing.Bounce.Out)  
             .delay(2000)
             .start();
+
+        toast2.position.set(toast2.position.x,toast2.position.y,toast2.position.z-1);      
 	}
 
 	return toaster;
